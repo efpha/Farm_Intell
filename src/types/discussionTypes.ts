cdda2b41b2b88ce8d9d85109654a4b8ee7a9a632
KEFFA <muthurikeffa254@gmail.com>
2026-03-04 00:42:55 +0300

    Typo on category instead of Category

--- src/types/discussionTypes.ts
+++ src/types/discussionTypes.ts
@@ -4,7 +4,7 @@ export type Thread = {
   content: string;
   created_at: string;
   reply_count?: number;
-  categories: { name: string } | null;
+  Categories: { name: string } | null; //in the DB category_id is a foreign key to the Categories table, so we can fetch the category name using a join
   profiles?: { username: string } | null;
 };
 
