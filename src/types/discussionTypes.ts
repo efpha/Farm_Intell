f99cdbcf178d3154057e5db326bf00a1a6160f1d
KEFFA <muthurikeffa254@gmail.com>
2026-04-26 22:07:21 +0300

    Add mediaUrls type

--- src/types/discussionTypes.ts
+++ src/types/discussionTypes.ts
@@ -4,6 +4,7 @@ export type Thread = {
   content: string;
   created_at: string;
   reply_count?: number;
+  media_urls?: string[] | null;
   Categories: { name: string } | null; //in the DB category_id is a foreign key to the Categories table, so we can fetch the category name using a join
   profiles?: { username: string } | null;
 };
